3f2c7a4c7635f823be07d5ffa2947d467839f45c
Aaron <aaronpiolang@gmail.com>
2019-12-09 23:12:59 +0000

    adding input form - still need to wire up post

--- express_server.js
+++ express_server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const generateRandomURL = () => {
+  const generatedURL = Math.random()
+    .toString(36)
+    .substring(7);
+  return generatedURL;
+};
+
+console.log(generateRandomURL());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -12,6 +23,15 @@ app.set("view engine", "ejs");
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
